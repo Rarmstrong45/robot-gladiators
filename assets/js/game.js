65d54010d47569879cd17ceddaae2a9349071886
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-04-04 17:03:12 -0500

    added variables

---
+++ assets/js/game.js
@@ -0,0 +1,22 @@
+// this creates a funtion named "fight"
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quoatation marks around playerName
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+
+
+
+//fight();
